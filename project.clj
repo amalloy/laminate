a7f647fae9e10e1de02d98a10225d4934c3acb5b
Alan Malloy <alan@malloys.org>
2013-05-29 14:24:10 -0700

    Accurate project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject laminate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+(defproject org.flatland/laminate "1.0.0"
+  :description "Some custom operators for the lamina query-streams language"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lamina "0.5.0-rc2"]])
