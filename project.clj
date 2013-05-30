db7a5e3a7fe6b84c68a99981e291490a2d11d20c
Alan Malloy <alan@malloys.org>
2013-05-30 16:59:41 -0700

    Version 1.2.0, with timezone support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/laminate "1.1.0"
+(defproject org.flatland/laminate "1.2.0"
   :description "Some custom operators for the lamina query-streams language"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.flatland/useful "0.9.4"]
