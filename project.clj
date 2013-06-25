faecdd53fde24f48728428b0b9efd71149eef77f
Alan Malloy <alan@malloys.org>
2013-06-24 17:01:07 -0700

    Better :description in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.flatland/laminate "1.3.0"
-  :description "Some custom operators for the lamina query-streams language"
+  :description "A utility library for lamina applications, including some custom operators for the lamina query-streams language."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.flatland/useful "0.9.4"]
                  [lamina "0.5.0-rc3"]])
