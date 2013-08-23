ad2c0e2a57c4c844052fefbcf330fc6d745faa2e
Alan Malloy <alan@malloys.org>
2013-08-23 16:17:36 -0700

    1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/laminate "1.3.2"
+(defproject org.flatland/laminate "1.4.0"
   :description "A utility library for lamina applications, including some custom operators for the lamina query-streams language."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.flatland/useful "0.9.4"]
