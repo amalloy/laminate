d8bd98ca0f47524913f303a2513d54732d23413c
Alan Malloy <alan@malloys.org>
2013-09-04 16:51:03 -0700

    New version of useful

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.flatland/laminate "1.5.1"
   :description "A utility library for lamina applications, including some custom operators for the lamina query-streams language."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.9.4"]
+                 [org.flatland/useful "0.10.3"]
                  [lamina "0.5.0"]])
